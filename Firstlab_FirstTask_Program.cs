bff56c89f8cddd9abdb965a994e6bc577c72773c
MaksymMats <45902502+MaksymMats@users.noreply.github.com>
2020-03-10 13:36:45 +0200

    Add files via upload

---
+++ Firstlab_FirstTask_Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1
+{
+    static class myclass
+    {
+       static Dictionary<char, int> mylist = new Dictionary<char, int>();
+        static char Low;
+       public static void SymbolsCount(string text)
+        {
+            mylist.Clear();
+            foreach (var ch in text)
+            {
+                Low = ch;
+                if (mylist.ContainsKey(Low))
+                    mylist[Low]++;
+                else
+                    mylist.Add(Low, 1);
+            }
+        }
+        public static Dictionary<char, double> Showprobability(Dictionary<char, double> symbols,int size)
+        {
+            foreach(var item in mylist)
+            {
+                symbols.Add(item.Key, ((double)item.Value/size));             
+            }
+            return symbols;
+        }
+        public static double Entrophy(Dictionary<char,double> symbols)
+        {
+            double entrophy=0;
+            foreach(var item in symbols)
+            {
+                entrophy += (item.Value * Math.Log(item.Value, 2));
+            }
+            return (entrophy * -1);
+        }
+      
+    }
+    class Program
+    {
+        static string Take_file(string path)
+        {
+            string text;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                text = sr.ReadToEnd();
+            }
+            return text;
+        }
+        static void Showdictionary(Dictionary<char,double> symbols)
+        {
+            foreach (var item in symbols)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+        }
+        static double Amountofinfo(int amount,double entro)
+        {
+            return amount * entro;
+        }
+        static void AmountOut(double amount, string path)
+        {
+            FileInfo file = new FileInfo(path);
+            double size = file.Length;
+            Console.WriteLine("Кількість інформації в тексті - {0} біт\nЗагальний розмір файлу - {1} біт\nРозмір файлу в - {2} рази більший за кількість інформації", amount, size * 8, size * 8 / amount);
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding(1251);
+            Dictionary<char, double> first = new Dictionary<char, double>();
+            Dictionary<char, double> second = new Dictionary<char, double>();
+            Dictionary<char, double> third = new Dictionary<char, double>();
+            string first_path = @"C:\Users\maksi\Desktop\КС\FirstLab\first_base64.txt";
+            string second_path = @"C:\Users\maksi\Desktop\КС\FirstLab\second_base64.txt";
+            string third_path = @"C:\Users\maksi\Desktop\КС\FirstLab\third_base64.txt";
+            myclass.SymbolsCount(Take_file(first_path));
+            myclass.Showprobability(first, Take_file(first_path).Length);
+            myclass.SymbolsCount(Take_file(second_path));
+            myclass.Showprobability(second, Take_file(second_path).Length);
+            myclass.SymbolsCount(Take_file(third_path));
+            myclass.Showprobability(third, Take_file(third_path).Length);
+            Showdictionary(first);
+            Console.WriteLine($"Entrophy {myclass.Entrophy(first)}");
+            AmountOut(Amountofinfo(Take_file(first_path).Length, myclass.Entrophy(first)), first_path);
+            Showdictionary(second);
+            Console.WriteLine($"Entrophy {myclass.Entrophy(second)}");
+            AmountOut(Amountofinfo(Take_file(second_path).Length, myclass.Entrophy(second)), second_path);
+            Showdictionary(third);
+            Console.WriteLine($"Entrophy {myclass.Entrophy(third)}");
+            AmountOut(Amountofinfo(Take_file(third_path).Length, myclass.Entrophy(third)), third_path);
+        }
+    }
+}
---
+++ Lab1_secondt_ask.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab1_part2
+{
+    class Program
+    {
+        static readonly char[] base64Table = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+                                                       'P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d',
+                                                       'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                                                       't','u','v','w','x','y','z','0','1','2','3','4','5','6','7',
+                                                       '8','9','+','/','=' };
+        static byte[] Take_file(string path)
+        {
+            string text;
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                text = sr.ReadToEnd();
+            }
+            byte[] mass = Encoding.UTF8.GetBytes(text);
+            return mass;
+        }
+
+        public static void ConvertToBase64(byte[] mass)
+        {
+            string binaryfortext = string.Join("", mass.Select(x => Convert.ToString(x, 2).PadLeft(8, '0')));
+            int countsbinary = binaryfortext.Count();
+            string concate = countsbinary % 3 == 1 ? "=" : countsbinary % 3 == 2 ? "==" : "";
+            int removedivision = countsbinary % 6;
+            if(removedivision != 0)
+                for(int i = 0; i < 6 - removedivision; i++) 
+                {
+                    binaryfortext = binaryfortext.Insert(countsbinary, "0");
+                    countsbinary++;
+                }
+            List<string> newList = Enumerable.Range(0, countsbinary / 6).Select(x => binaryfortext.Substring(x * 6, 6)).ToList();
+            string finaltext = string.Join("", newList.Select(x => base64Table[Convert.ToByte(x,2)]))+concate;
+            Console.WriteLine($"\n\n\n{finaltext}");
+        }
+        static void Converttobase64(string path)
+        {
+            string text = Convert.ToBase64String(Take_file(path));
+            Console.WriteLine($"Compare with default method \n\n\n{text}");
+            using (StreamWriter sw = new StreamWriter(path.Replace(".rar", "_base64.txt"), false))
+            {
+                sw.WriteLine(text);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            string first_path = @"C:\Users\maksi\Desktop\КС\FirstLab\first.rar";
+            string second_path = @"C:\Users\maksi\Desktop\КС\FirstLab\second.rar";
+            string third_path = @"C:\Users\maksi\Desktop\КС\FirstLab\third.rar";
+            ConvertToBase64(Take_file(first_path));
+            Converttobase64(first_path);
+            ConvertToBase64(Take_file(second_path));
+            Converttobase64(second_path);
+            ConvertToBase64(Take_file(third_path));
+            Converttobase64(third_path);
+        }
+    }
+}
